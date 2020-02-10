144d15f9ed6d8fffafcb3939157d7375511f1948
timmens <tim@mensinger96.com>
2020-02-10 16:57:10 +0100

    Add node identification using counter.

--- causaltree.py
+++ causaltree.py
@@ -1,26 +1,8 @@
 # Module to fit a causal tree on numpy arrays using numba
-# TODO:
-#  1) Check if it works
-#  2) numba implementation
+# TODO: see issues on github
+from itertools import count
 
 import numpy as np
-
-
-# Delete?
-#
-#def _compute_maximum_number_nodes(n, min_leaf):
-#    """
-#    Computes the maximum number of nodes in a valid binary tree when there are
-#    `n` observations and the minimum number of observations (of one treatment
-#    status) is set to `min_leaf`.
-#
-#    :param n: number of observations in a data set
-#    :param min_leaf: minimum number of obs. in a leaf of one treatment type
-#    :return: (int): maximum number of tree nodes (including the root)
-#    """
-#    max_leafs = np.floor(n / (2 * min_leaf))
-#    max_levels = np.ceil(np.log2(max_leafs)).astype('int')
-#    return 2 ** (max_levels + 1) - 1
 
 
 def _compute_child_node_ids(parent_id):
@@ -135,14 +117,14 @@ def _retrieve_index(index, index_sorted, split_index):
     :param split_index:
     :return: three dim tuple
     """
-    left = index_sorted[:(split_index + 1)]
-    right = index_sorted[(split_index + 1):]
+    left = index_sorted[: (split_index + 1)]
+    right = index_sorted[(split_index + 1) :]
     nonzero_index = np.nonzero(index)[0]
 
     n = len(index)
 
-    left_index = np.full((n, ), False)
-    right_index = np.full((n, ), False)
+    left_index = np.full((n,), False)
+    right_index = np.full((n,), False)
     left_index[nonzero_index[left]] = True
     right_index[nonzero_index[right]] = True
     global_split_index = nonzero_index[index_sorted[split_index]]
@@ -151,7 +133,7 @@ def _retrieve_index(index, index_sorted, split_index):
 
 
 def _find_optimal_split(
-        y, t, x, index, metric, loss_weighting, min_leaf, level
+    y, t, x, index, metric, loss_weighting, min_leaf, level
 ):
     """
     Given the data (`y`, `t`, `x`) finds optimal splitting point in the
@@ -188,14 +170,14 @@ def _find_optimal_split(
         for i in splitting_points:
             # loop through observations
 
-            left_te = _estimate_treatment_effect(yy[:(i+1)], tt[:(i+1)])
-            right_te = _estimate_treatment_effect(yy[(i+1):], tt[(i+1):])
+            left_te = _estimate_treatment_effect(yy[: (i + 1)], tt[: (i + 1)])
+            right_te = _estimate_treatment_effect(yy[(i + 1) :], tt[(i + 1) :])
 
-            left_loss = metric(yy_transformed[:(i+1)], left_te)
-            right_loss = metric(yy_transformed[(i+1):], right_te)
+            left_loss = metric(yy_transformed[: (i + 1)], left_te)
+            right_loss = metric(yy_transformed[(i + 1) :], right_te)
 
             global_loss = loss_weighting(
-                left_loss, right_loss, i+1, len(yy)-i-1
+                left_loss, right_loss, i + 1, len(yy) - i - 1
             )
             if global_loss < loss:
                 split_var = j
@@ -221,10 +203,19 @@ def _find_optimal_split(
     return left, right, split_information
 
 
-def _fit(
-        y, t, x, index,
-        metric, loss_weighting, min_leaf,
-        level=0, nodeid=0):
+def _fit_node(
+    y,
+    t,
+    x,
+    index,
+    metric,
+    loss_weighting,
+    min_leaf,
+    max_depth,
+    counter,
+    level=0,
+    nodeid=0,
+):
     """
     Core function which recursively splits the feature space until stopping
     criterium is reached (`min_leaf`) and returns splitting information.
@@ -247,8 +238,8 @@ def _fit(
         y, t, x, index, metric, loss_weighting, min_leaf, level
     )
 
-    if tmp is None:
-        # if we do not split, the node must be a leaf, hence we add the
+    if tmp is None or level == max_depth:
+        # if we do not split the node must be a leaf, hence we add the
         # treatment effect
         te = _estimate_treatment_effect(y[index], t[index])
         out = np.array(4 * [np.nan] + [level, te, nodeid]).reshape((-1, 7))
@@ -258,17 +249,34 @@ def _fit(
         split_information[6] = nodeid
         out = np.concatenate((out, split_information.reshape((-1, 7))), axis=0)
 
-        leftid, rightid = _compute_child_node_ids(nodeid)
+        leftid = next(counter)
+        rightid = next(counter)
 
-        out_left = _fit(
-            y, t, x, left,
-            metric, loss_weighting,
-            min_leaf, level + 1, leftid
+        out_left = _fit_node(
+            y,
+            t,
+            x,
+            left,
+            metric,
+            loss_weighting,
+            min_leaf,
+            max_depth,
+            counter,
+            level + 1,
+            leftid,
         )
-        out_right = _fit(
-            y, t, x, right,
-            metric, loss_weighting,
-            min_leaf, level + 1, rightid
+        out_right = _fit_node(
+            y,
+            t,
+            x,
+            right,
+            metric,
+            loss_weighting,
+            min_leaf,
+            max_depth,
+            counter,
+            level + 1,
+            rightid,
         )
 
         if out_left.size != 0:
@@ -279,7 +287,9 @@ def _fit(
         return out
 
 
-def fitcausaltree(y, t, x, min_leaf=3, metric=None, loss_weighting=None):
+def fit_causaltree(
+    y, t, x, min_leaf=3, max_depth=25, metric=None, loss_weighting=None
+):
     """
     Wrapper function for core function _fit.
 
@@ -288,32 +298,63 @@ def fitcausaltree(y, t, x, min_leaf=3, metric=None, loss_weighting=None):
     :param x: (n,p) np.array containing independent variables
     :param min_leaf: minimum number of either treated or untreated observations
                     to be in a leaf after the split
+    :param max_depth: maxmimum number of allowed levels
     :param metric: a distance function, e.g. l2 loss
     :param loss_weighting: a function which computes the global loss of a split
                         by combining the losses in each subset using weights
                         given by the no. of obersavtions falling in the
                         respective subset.
     :return:
-    TODO:
-        1) Implement assert statements for data types
     """
     n = len(y)
     index = np.full((n,), True)
 
     if metric is None:
+
         def metric(outcomes, estimate):
             return np.sum((outcomes - estimate) ** 2)
 
     if loss_weighting is None:
+
         def loss_weighting(left_loss, right_loss, w1, w2):
             return left_loss + right_loss
 
-    tree = _fit(y, t, x, index, metric, loss_weighting, min_leaf)
+    # initialize counter object
+    counter = count()
+    rootid = next(counter)
+
+    # fit tree
+    tree = _fit_node(
+        y,
+        t,
+        x,
+        index,
+        metric,
+        loss_weighting,
+        counter,
+        min_leaf,
+        max_depth,
+        0,
+        rootid,
+    )
 
     return tree
 
 
-def predictcausaltree(ctree, x):
+def _does_node_have_children(ctree, node_id):
+    """
+    Returns `True` if node `node_id` has children in tree `ctree` and False
+    else.
+
+    :param ctree:
+    :param node_id:
+    :return:
+    """
+    left, _ = _compute_child_node_ids(node_id)
+    return np.isin(ctree[:, 6], left)
+
+
+def predict_causaltree(ctree, x):
     """
     Predicts new outcomes using a fitted causal tree `ctree` from new features
     `x`.
@@ -322,4 +363,16 @@ def predictcausaltree(ctree, x):
     :param x: new covariates from which we want to predict outcomes
     :return: predicted outcomes
     """
-    pass
+    node = 0
+
+    has_children = _does_node_have_children(ctree, node)
+    while has_children:
+        var, value = ctree[node, 0], ctree[node, 1]
+        if x[var] <= value:
+            node, _ = _compute_child_node_ids(node)
+        else:
+            _, node = _compute_child_node_ids(node)
+        has_children = _does_node_have_children(ctree, node)
+
+    out = ctree[node, 5]
+    return out
